8bcc872346d1b2eae783a39cbe5613c7e2d78411
kcmurray1 <kristiancmurray@gmail.com>
2024-07-03 11:32:44 -0700

    updated to Python311

--- tests/test.py
+++ tests/test.py
@@ -1,5 +1,5 @@
 from transcriber.transcript import TranscriptProcessor
-from tests.test_resources import test_data
+from tests.test_resources import test_data, TestResult
 import unittest
 
 class TestTranscriber(unittest.TestCase):
@@ -8,25 +8,27 @@ class TestTranscriber(unittest.TestCase):
 
     def test_a_video_retrieval_count(self):
         for test_datum in test_data:
+            self.test_results[test_datum.channel] = TestResult()
             retrieved_videos = self.test_transcriber.find_videos(test_datum.url)
             self.assertEquals(test_datum.vids, len(retrieved_videos))
-            self.test_results[test_datum.channel] = retrieved_videos
+            self.test_results[test_datum.channel].videos = retrieved_videos
     
-    def test_b_find_phrase(self):
+    def test_b_find_phrase_match_count(self):
         for test_datum in test_data:
-            test_videos = self.test_results[test_datum.channel]
+            test_videos = self.test_results[test_datum.channel].videos
             num_test_matches, num_errors = self.test_transcriber.channel_search(test_videos, author=test_datum.channel, phrase=test_datum.phrase)
             self.assertAlmostEquals(test_datum.match_count, num_test_matches, delta=3)
 
 
 
 def start_tests():
+    # Store multiple test classes(If necessary)
     test_classes = [TestTranscriber]
     test_loader = unittest.TestLoader()
     suites = [test_loader.loadTestsFromTestCase(test_class) for test_class in test_classes]
     suites = unittest.TestSuite(suites)
 
-    # run all tests with verbosity 
+    # run all tests
     unittest.TextTestRunner(verbosity=2).run(suites)
 
 if __name__ == '__main__':
--- tests/test_resources.py
+++ tests/test_resources.py
@@ -6,9 +6,14 @@ instance_attributes = ['channel', 'url', 'phrase', 'vids', 'match_count']
 class InstanceConfig(collections.namedtuple('InstanceConfig', instance_attributes)):
     def __str__(self):
         return f'{self.channel}, {self.vids} Videos'
+    
+class TestResult():
+    def __init__(self):
+        self.num_errors = 0
+        self.videos = None
 
 test_data = [ 
     InstanceConfig(channel="jdh", url="https://www.youtube.com/@jdh/videos", phrase="hello", match_count=10, vids=23),
-    InstanceConfig(channel="git-amend", url="https://www.youtube.com/@git-amend/videos", phrase="hello", vids=68),
+    # InstanceConfig(channel="git-amend", url="https://www.youtube.com/@git-amend/videos", phrase="hello", vids=68),
     
 ]
